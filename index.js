77028525c41b6b35c3080f8241030d1267a934ed
Avneesh Dev <avneesh.dev@gmail.com>
2023-03-31 08:43:16 -0400

    Phase1 Arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 = 2
+let num2 = 31
+let multiply  = num1 * num2
+console.log(multiply)
 
-// Write your code here
+let random = Math.floor(Math.random()*10)
+console.log(random)
+
+let num3 = 16
+let num4 = 4
+let mod  = num3 / num4
+console.log(mod)
+
+
+let num5 = 20
+let num6 = 10
+let max = Math.max(num5, num6)
+console.log(max)
